1b5068b965ec72759eabeba113a450e917322509
Sam Reither <samsreither@gmail.com>
2024-10-28 18:08:18 -0400

    sprint 10 first submit

---
+++ vite-project/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+function Footer () {
+    return (
+        <div className="footer">
+            <p className="footer__name">Developed by Sam Reither</p>
+            <p className="footer__year">2024</p>
+        </div>
+        
+    );
+}
+
+export default Footer;
